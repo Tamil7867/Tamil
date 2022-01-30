3ca6e07047ef21d7ad3a06630b9b9569f0ba9b45
Tamil7867 <tamilselvan.john@gmail.com>
2022-01-30 13:30:23 +0530

    Username Functionality

--- src/org/libglobal/Login.java
+++ src/org/libglobal/Login.java
@@ -1,5 +1,10 @@
 package org.libglobal;
 
 public class Login {
-
+	public static void userName() {
+		System.out.println("Username");
+	}
+	public static void main(String[] args) {
+		userName();
+	}
 }
